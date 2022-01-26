5660cf759e0cf45238ab05bc0b09867ab99d2a8c
Zhiqiagdaniel.Xu <Zhiqiagdaniel.Xu@ontario.ca>
2022-01-25 19:10:20 -0500

    Asynchronous Resolvers

--- src/main/java/com/springgraphql/tutorial/resolver/bank/ClientResolver.java
+++ src/main/java/com/springgraphql/tutorial/resolver/bank/ClientResolver.java
@@ -9,28 +9,40 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Slf4j
 @Component
 public class ClientResolver implements GraphQLResolver<BankAccount> {
 
-    public DataFetcherResult<Client> client(BankAccount bankAccount) {
-        log.info("Requesting client data for bank account id {}", bankAccount.getId());
+    private final ExecutorService executorService = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors()
+    );
+
+    public CompletableFuture<Client> client(BankAccount bankAccount) {
+//        log.info("Requesting client data for bank account id {}", bankAccount.getId());
 
 //        throw new GraphQLException("Client unavailable!");
 //        throw new RuntimeException("Server Internal Error!");
 
-        return DataFetcherResult.<Client>newResult()
-                .data(Client.builder().id(UUID.randomUUID())
-                .firstName("Stephen")
-                .lastName("Curry")
-                .build())
-            .error(new GenericGraphQLError("Could not get sub-client id"))
-            .build();
-
-//        return Client.builder().id(UUID.randomUUID())
+//        return DataFetcherResult.<Client>newResult()
+//                .data(Client.builder().id(UUID.randomUUID())
 //                .firstName("Stephen")
 //                .lastName("Curry")
-//                .build();
+//                .build())
+//            .error(new GenericGraphQLError("Could not get sub-client id"))
+//            .build();
+        log.info("Stop me debugging");
+        return CompletableFuture.supplyAsync(
+                () -> {
+                    log.info("Requesting client data for bank account id {}", bankAccount.getId());
+                    return Client.builder().id(UUID.randomUUID())
+                        .firstName("Stephen")
+                        .lastName("Curry")
+                        .build();
+                },
+                executorService);
     }
 }
