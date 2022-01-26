9d2b71d662e7e7e99d16e25fc276170ab434b35b
Zhiqiagdaniel.Xu <Zhiqiagdaniel.Xu@ontario.ca>
2022-01-26 16:20:32 -0500

    DataFetchingEnvironment, SelectionSet

--- src/main/java/com/springgraphql/tutorial/resolver/bank/query/BankAccountResolver.java
+++ src/main/java/com/springgraphql/tutorial/resolver/bank/query/BankAccountResolver.java
@@ -4,18 +4,21 @@ import com.springgraphql.tutorial.domain.bank.BankAccount;
 import com.springgraphql.tutorial.domain.bank.Client;
 import com.springgraphql.tutorial.domain.bank.Currency;
 import graphql.kickstart.tools.GraphQLQueryResolver;
+import graphql.schema.DataFetchingEnvironment;
+import graphql.schema.SelectedField;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
 public class BankAccountResolver implements GraphQLQueryResolver {
 
-    public BankAccount bankAccount(UUID id) {
+    public BankAccount bankAccount(UUID id, DataFetchingEnvironment environment) {
         log.info("Retrieving bank account id: {}", id);
         List<String> middleNames = Arrays.asList(new String[] {"Mid1", "Mid2"});
 //        Client clientA = Client.builder().id(UUID.randomUUID()).firstName("ClientA")
@@ -24,6 +27,12 @@ public class BankAccountResolver implements GraphQLQueryResolver {
 //                .lastName("Jordan").middleNames(middleNames).build();
 //        clientA.setClient(clientB);
 //        clientB.setClient(clientA);
+
+        var requestedFields = environment.getSelectionSet().getFields().stream()
+                .map(SelectedField::getName).collect(Collectors.toUnmodifiableSet());
+
+        log.info("Contains filed \"firstName\"? {}", requestedFields.contains("firstName"));
+
         return BankAccount.builder().id(id).currency(Currency.CAD).build();
     }
 }
